c4d2b4284d98cd7dc39c54c039cc347cec16edfb
MadsonPereira <madson.ribas@gmail.com>
2022-03-11 22:44:04 -0300

    Projeto Blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,26 +1,7 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
-//1
-
 let mensagem = "Boas vindas ao jogo de Blackjack!"
 console.log(mensagem)
 
-//2
-
 let inicio = confirm("Quer iniciar uma nova rodada?")
-
-//3
 
 let usuario = []
 let computador = []
@@ -35,8 +16,6 @@ if (inicio) {
 } else {
     console.log("O jogo acabou")
 }
-
-console.log("usuario: " + somausu, "computador: " + somacomp);
 
 if (somausu > somacomp) {
     console.log("O Usuário ganhou!");
