9a401c6e6fd07793c7ccd12bb8ebbaa46e5b6d6b
MadsonPereira <madson.ribas@gmail.com>
2022-05-29 22:04:06 -0300

    Melhorias do Findê

--- modulo3/projeto-labefy/labefy/src/Components/Lista.jsx
+++ modulo3/projeto-labefy/labefy/src/Components/Lista.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React from "react";
 import styled from 'styled-components';
+import Musicas from "./Musicas";
+
 
 const Main = styled.div`
     display: flex;
@@ -30,9 +32,8 @@ const ListaContainer = styled.div`
     min-width: 300px
 `
 const UsuarioContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: 200px 50px 50px;
     background-color: #097F00;
     border: 1px solid black;
     border-radius: 10px;
@@ -42,15 +43,54 @@ const BotaoDelete = styled.button`
     color: red;
     margin: 10px
 `
-const UsuarioP = styled.p`
+const BotaoAdd = styled.button`
+    color: blue;
+    margin: 10px
+`
+const UsuarioP = styled.button`
     color: #D1D1E6;
     margin: 10px;
+    border: none;
+    outline: none;
+    background-color: #097F00;
+    cursor: pointer;
 `
 
 export default class Lista extends React.Component {
 
     state = {
-        playlist: []
+        AddMusica: false,
+        playlist: [],
+        name: "",
+        artist: "",
+        url: ""
+    }
+    AddMusicas = (id) => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
+        const body = {
+            "name": this.state.name,
+            "artist": this.state.artist,
+            "url": this.state.url
+        }
+        axios.post(url, body, {
+            headers:{
+            Authorization: "madson-pereira-hopper"
+            }
+        }).then(()=> {alert("Música adicionada à sua Playlist!")})
+        .catch((err) => {alert("Falha ao adicionar música")})
+        .finally(() => {
+            this.setState({name: "", artist: "", url: ""})
+        })
+    }
+    onChangeName = (event) => {
+        console.log(this.state.name);
+        this.setState({name: event.target.value})
+    }
+    onChangeArtist = (event) => {
+        this.setState({artist: event.target.value})
+    }
+    onChangeURL = (event) => {
+        this.setState({url: event.target.value})
     }
 
     componentDidMount() {
@@ -95,6 +135,14 @@ export default class Lista extends React.Component {
                 {/* <button onClick={this.props.filhoDois}>Voltar</button> */}
                 <Titulo>Playlists</Titulo>
                 </Header>
+                <Musicas
+                onChangeName={this.onChangeName}
+                onChangeArtist={this.onChangeArtist}
+                onChangeURL={this.onChangeURL}
+                name={this.state.name}
+                artist={this.state.artist}
+                url={this.state.url}
+                />
                 <UsuariosContainer>
                     <h3>Suas Playlists</h3>
                     {
@@ -105,6 +153,7 @@ export default class Lista extends React.Component {
                                         <UsuarioP>
                                             { playlist.name }
                                         </UsuarioP>
+                                        <BotaoAdd onClick={() => {if (window.confirm("Adicionar música à sua playlist?")){this.AddMusicas(playlist.id)}}}>+</BotaoAdd>
                                         <BotaoDelete onClick={() => {if(window.confirm("Deseja deletar esta playlist?")){this.delPlaylist(playlist.id)}}}>X</BotaoDelete>
                                     </UsuarioContainer>
                                 </ListaContainer>
